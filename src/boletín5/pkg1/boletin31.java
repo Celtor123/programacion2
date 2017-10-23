83c8b63571510741de99e266fab12dad38b19f9d
oracle <oracle@victoria24.danielcastelao.org>
2017-10-23 10:16:35 +0200

    Casi está

---
+++ src/boletín5/pkg1/boletin31.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletín5.pkg1;
+
+/**
+ *
+ * @author oracle
+ */
+public class boletin31 {
+//Temos a seguinte clase Coche:
+public class Coche {
+     private int velocidade ;
+
+    public Coche(){
+    velocidade =0 ;
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+}
+    
+}
+
+
+//Engade a clase Coche os seguintes métodos :
+
+//public int getVelocidade() . Este método devolve a velocidade actual
+
+//public void acelerar (int valor) .Este método incrementa a velocidade na cantidade valor
+
+//public void frenar (int menos).Este método diminue a velocidade na cantidade menos.
+
+//Crea a clase Boletin 5.1 para comprobar que o programa se executa ben .dandolle os valores que precises.
